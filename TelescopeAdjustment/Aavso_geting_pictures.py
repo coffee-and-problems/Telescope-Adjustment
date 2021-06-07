2148b06a430aded61342a2284e348a11b8dba665
Maria <marychobanu@gmail.com>
2021-06-07 09:56:56 +0300

    Added function descriptions

--- TelescopeAdjustment/Aavso_geting_pictures.py
+++ TelescopeAdjustment/Aavso_geting_pictures.py
@@ -12,12 +12,24 @@ maglimit = '20.0'
 fov = 15.0
 
 def SendRequest(ra, dec):
+    """
+    Sends a request to https://app.aavso.org to get information on a specific object.
+        :param ra: RA of the object (ex. 18 06 50.7 or 18:26:15.0)
+        :type ra: string
+        :param dec: dec of the object (ex. +69 49 28 or -14:51:00)
+        :type dec: string
+    """
     PARAMS = {'ra':ra, 'dec':dec, 'maglimit':maglimit, 'fov':fov,
               'dss':'True', 'special':'std_field', 'std_field':'on',
               'north':'up', 'east':'right'}
     return requests.get(url = URL, params = PARAMS)
 
 def GetPictureURL(response):
+    """
+    Parses a response for an immage URL. If multiple, the first image will be used.
+        :param response: http response from aavso web cite
+        :type response: requests.models.Response
+    """
     soup = BeautifulSoup(response.text)
 
     img_responsive = soup.find_all("img", class_="img-responsive")
@@ -26,20 +38,42 @@ def GetPictureURL(response):
         return None
     return domen + img_responsive[0].attrs['src']
 
-def GetPicture(ra, dec):
+def GetField(obj_name, ra, dec):
+    """
+    Returns DSS field of view of the coords specified
+        :param obj_name: name of the object
+        :type ra: string
+        :param ra: RA of the object (ex. 18 06 50.7 or 18:26:15.0)
+        :type ra: string
+        :param dec: dec of the object (ex. +69 49 28 or -14:51:00)
+        :type dec: string
+    """
     response = SendRequest(ra, dec)
     picture_url = GetPictureURL(response)
     if picture_url is None:
+        print("No field found for {0}".format(obj_name)) #сделать нормальное логирование
         return None
-    return requests.get(url = picture_url).content
+    return Crop_image(requests.get(url = picture_url).content)
 
-def crop_image(picture):
+def Crop_image(picture):
+    """
+    Returns only field of view from aavso image
+        :param picture: the image
+        :type picture: bytes string
+    """
     _pil = Image.open(io.BytesIO(picture))
     return _pil.crop((19, 182, 1183, 1350))
 
 def Save(object_name, picture):
+    """
+    Saves pictures in "Fields" directory
+        :param object_name: name of a file to be created
+        :type object_name: string
+        :param picture: the FOV to be saved
+        :type picture: PIL.Image.Image
+    """
     file_path = path.join("Fields", "{0}.png".format(object_name))
-    crop_image(picture).save(file_path)
+    picture.save(file_path)
 
 
 source_file = "test_coords.csv"
@@ -49,9 +83,7 @@ with open(source_file) as file:
     reader = csv.reader(file, skipinitialspace=True)
     next(reader, None)
     for source in reader:
-        field = GetPicture(source[1], source[2])
-        if field is None:
-            print("No field found for {0}".format(source[0])) #сделать нормальное логирование
-
-        Save(source[0], field)
+        field = GetField(source[0], source[1], source[2])
+        if field is not None:
+            Save(source[0], field)
 
--- TelescopeAdjustment/main.py
+++ TelescopeAdjustment/main.py
@@ -4,33 +4,53 @@ import glob
 from os import path
 
 from lib import alipylocal as alipy
-verbose = False
+verbose = True
 
-test_fits = r"C:\Users\maryc\source\repos\TelescopeAdjustment\TelescopeAdjustment\Fields\fits\OC457.fits"
+test_fits = r"C:\Users\maryc\source\repos\TelescopeAdjustment\TelescopeAdjustment\observations\3c371iS6.FIT"
 
 path_to_reference_fields = r"C:\Users\maryc\source\repos\TelescopeAdjustment\TelescopeAdjustment\Fields"
 path_to_reference_fits = r"C:\Users\maryc\source\repos\TelescopeAdjustment\TelescopeAdjustment\Fields\fits"
 path_to_reference_catalogs = r"C:\Users\maryc\source\repos\TelescopeAdjustment\TelescopeAdjustment\alipy_cats"
 
 def make_fits(png_path):
+    """
+    Converts .png image to .fits one and places it to "Fields\fits"
+        :param png_path: path to png image to be converted
+        :type png_path: string
+    """
     for im_path in glob.glob(path.join(png_path, "*.png")):
         name = path.basename(im_path)[:-4]
         im = imageio.imread(im_path)
         alipy.align.tofits("{0}\{1}.fits".format(path_to_reference_fits, name), im)
 
 def make_cat(path):
+    """
+    Creates ImgCat catalog. Places it to "alipy_cats" if not presented, then loads it to memory.
+    Uses SExtractor
+        :param path: path to fits file
+        :type path: string
+    """
     cat = alipy.imgcat.ImgCat(path)
     cat.makecat(rerun=False, keepcat=True, verbose=verbose)
     cat.makestarlist(verbose=verbose)
     return cat
 
 def make_reference_cats():
+    """
+    Creates ImgCat catalogs for all files in "Fields\fits" directory
+    """
     ref_cats = []
     for im_path in glob.glob(path.join(path_to_reference_fits, "*.fits")):
         ref_cats.append(make_cat(im_path))
     return ref_cats
 
 def find_transform(picture, object_name=None):
+    """
+    Returns Identification.trans (). The user can specify the name of reference object and the corresponding catalog will be used.
+    If none, all catalogs will be viewed till one is OK.
+        :param object_name: object name (ex. Q1959)
+        :type object_name: string
+    """
     if object_name is None:
         refs = make_reference_cats()
     else:
@@ -42,6 +62,9 @@ def find_transform(picture, object_name=None):
         idn.findtrans(verbose=verbose)
         if idn.ok:
             return idn.trans
+        return None
 
 #make_fits(path_to_reference_fields)
-tarns = find_transform(test_fits)
+tarns = find_transform(test_fits, "3C371")
+
+pass
