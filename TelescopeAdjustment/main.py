99a9247b20c87719981454ae333bb23c660e0824
Maria <marychobanu@gmail.com>
2021-06-20 22:03:00 +0300

    Fixed the code after the cleaning up :)

--- TelescopeAdjustment/main.py
+++ TelescopeAdjustment/main.py
@@ -13,7 +13,7 @@ from astropy.io import fits
 
 class Main():
     """Main class"""
-    def __init__(self, object_name, observation):
+    def __init__(self, object_name, observation, source_file):
         self.fov = 10
         self.observation = observation
         self.ref = object_name
@@ -21,37 +21,34 @@ class Main():
             reader = csv.reader(file, skipinitialspace=True)
             next(reader, None)
             for source in reader:
-                if (source[0] == ref):
+                if (source[0] == self.ref):
                     self.ra = source[1]
                     self.dec = source[2]
 
-    def run():
+    def run(self):
         #Geting Gaia catalogs
-        source_file = "test_coords.csv"
-        #source_file = "objects_coords.csv
-
         gaia = Gaia_data()
         adj = Adjustment(verbose=False, keepcat=True)
 
-        if not path.isfile(path.join(adj.path_to_reference_catalogs, f"{ref}alipysexcat")):
-            gaia.make_ref_cat(ref, ra, dec, fov)
+        if not path.isfile(path.join(adj.path_to_reference_catalogs, f"{self.ref}alipysexcat")):
+            gaia.make_ref_cat(ref, self.ra, self.dec, self.fov)
 
 
         #Finding transform for the field observed
-        trans = adj.find_transform(observation, ref)
+        trans = adj.find_transform(self.observation, self.ref)
 
         #Obtaining telescope adjustment in arcsecs
         shift = trans.get_shift()
 
         coords = gaia.coords_to_deg(self.ra, self.dec)
-        ratio_x, ratio_y = gaia.get_ratio(fov, coords)
+        ratio_x, ratio_y = gaia.get_ratio(self.fov, coords)
         ratio_x_arcsec = ratio_x / 3600
         ratio_y_arcsec = ratio_y / 3600
         print("x, y offset, pxl: {0}".format(shift))
         shift_ra = shift[0]/ratio_x_arcsec
         shift_dec = shift[1]/ratio_y_arcsec
         print("ra, dec offset, arcsec: {0}, {1}".format(shift_ra, shift_dec))
-        return (coords[0] + shift_ra, coords[1] + shift_dec)
+        return (coords[0] + shift[0]/ratio_x, coords[1] + shift[1]/ratio_y)
 
 #Plotting data
 #import matplotlib.pyplot as plt
@@ -79,4 +76,6 @@ class Main():
 #plt.plot(x_trans, y_trans, "rx")
 #plt.show()
 
-main = Main("q1959", "observations\q1959iS4.FIT")
+main = Main("Q1959", "observations\q1959iS4.FIT", "test_coords.csv")
+a = main.run()
+print(a)
